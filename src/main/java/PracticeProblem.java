f2066d9ba6f750239aac2ab98574c69e10fb5283
michael-chen-ics4u1-01-25 <michael.chen29@student.tdsb.on.ca>
2025-09-15 13:07:36 -0400

    completed set

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -5,5 +5,37 @@ public class PracticeProblem {
 	}
 
 	//Write your functions here
+	public static String evenOrOdd(int a) {
+		return a % 2 == 0 ? "Even" : "Odd";
+	}
 
+	public static String teacherOrStudent(String a) {
+		return a == "Kalisz" ? "Teacher" : "Student";
+	}
+
+	public static int fartherFromZero(int a) {
+		if (a < 0) {
+			return a - 5;
+		} else if (a > 0) {
+			return a + 5;
+		} else {
+			return 0;
+		}
+	}
+
+	public static String isFive(int a) {
+		return a == 5 ? "The number is Five" : "The number is not Five";
+	}
+
+	public static String positiveOrNegative(double a) {
+		return a > 0 ? "Positive" : "Negative";
+	}
+
+	public static String highOrLow(int a) {
+		return a > 100 ? "High" : "Low";
+	}
+
+	public static String isHello(String a) {
+		return a == "Hello" ? "The word is Hello" : "The word is not Hello";
+	}
 }
